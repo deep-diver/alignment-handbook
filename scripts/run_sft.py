a6087d6f17e382f3c48d49b9ebf8fd5f6c7b0f06
Chansung Park <deep.diver.csp@gmail.com>
2024-11-15 10:42:34 +0900

    Update run_sft.py

--- scripts/run_sft.py
+++ scripts/run_sft.py
@@ -200,14 +200,14 @@ def main():
     logger.info(f"Model saved to {training_args.output_dir}")
 
     # Save everything else on main process
-    kwargs = {
-        # "finetuned_from": model_args.model_name_or_path,
-        # "dataset": list(data_args.dataset_mixer.keys()),
-        # "dataset_tags": list(data_args.dataset_mixer.keys()),
-        "tags": ["alignment-handbook"],
-    }
+    # kwargs = {
+    #     # "finetuned_from": model_args.model_name_or_path,
+    #     # "dataset": list(data_args.dataset_mixer.keys()),
+    #     # "dataset_tags": list(data_args.dataset_mixer.keys()),
+    #     "tags": ["alignment-handbook"],
+    # }
     if trainer.accelerator.is_main_process:
-        trainer.create_model_card(**kwargs)
+        # trainer.create_model_card(**kwargs)
         # Restore k,v cache for fast inference
         trainer.model.config.use_cache = True
         trainer.model.config.save_pretrained(training_args.output_dir)
