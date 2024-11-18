4bc340ce62605e4e024387231c1bcb3e649d8285
chansung <deep.diver.csp@gmail.com>
2024-11-18 10:02:54 +0900

    add pissa support

--- src/alignment/model_utils.py
+++ src/alignment/model_utils.py
@@ -102,6 +102,8 @@ def get_peft_config(model_args: ModelArguments) -> PeftConfig | None:
     if model_args.init_lora_weights == "True":
         model_args.init_lora_weights = True
 
+    print(model_args.init_lora_weights)
+
     peft_config = LoraConfig(
         r=model_args.lora_r,
         lora_alpha=model_args.lora_alpha,
